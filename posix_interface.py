ce7bf06daa7a8b38f2651ccf4703ecfa17b620f1
David Brown <david.brown@pnnl.gov>
2015-07-25 15:59:02 -0700

    here's some more bits to split out files

--- posix_interface.py
+++ posix_interface.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python
 
-from hpss_ctypes import HPSSClient, HPSSFile
-
-CLIENT = HPSSClient(user="svc-myemsldev", auth="/home/dmlb2000/svc-myemsldev.keytab")
 block_size = 1<<12
 
 def myemsl_archiveinterface(env, start_response):
     if env['REQUEST_METHOD'] == 'GET':
-        myfile = CLIENT.open("/myemsldev%s"%(env['PATH_INFO']), "r")
+        myfile = open("/myemsldev%s"%(env['PATH_INFO']), "r")
         start_response('200 OK', [('Content-Type','application/octet-stream')])
         if 'wsgi.file_wrapper' in env:
             return env['wsgi.file_wrapper'](myfile, block_size)
--- server.py
+++ server.py
@@ -1,16 +1,29 @@
 #!/usr/bin/python
+"""
+MyEMSL Archive Interface
 
+This is the main program that starts the WSGI server.
+
+The core of the server code is in archive_interface.py.
+"""
 from argparse import ArgumentParser
-from archive_interface import myemsl_archiveinterface
+from archive_interface import myemsl_archiveinterface as hpss_srv
+from posix_interface import myemsl_archiveinterface as posix_srv
 from wsgiref.simple_server import make_server
 
+# 
 parser = ArgumentParser(description='Run the archive interface.')
 
 parser.add_argument('-p', '--port', metavar='PORT', type=int, 
-    nargs=1, required=True, dest='port')
+    nargs=1, default=8080, dest='port', 
+    help="port to listen on")
 parser.add_argument('-a', '--address', metavar='PORT', nargs=1, 
-    required=True, dest='address')
+    default='localhost', dest='address', 
+    help="address to listen on")
+parser.add_argument('-t', '--test', dest='server', action='store_const',
+    const=posix_srv, default=hpss_srv, 
+    help='use the posix testing backend')
 
 args = parser.parse_args()
-srv = make_server(args.address[-1], args.port[-1], myemsl_archiveinterface)
+srv = make_server(args.address[-1], args.port[-1], args.server)
 srv.serve_forever()
