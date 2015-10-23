28feb560605e52e905e003b6e9618f711e61ae59
dmlb2000 <dmlb2000@gmail.com>
2015-10-23 11:05:03 -0700

    remove old bits

--- old/autogen.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-libtoolize
-aclocal -I m4
-automake --add-missing --copy
-autoconf --force
--- old/myemslarchive.h
+++
@@ -1,8 +0,0 @@
-#ifndef MYEMSL_ARCHIVE_H
-#define MYEMSL_ARCHIVE_H
-
-char *id2dirandfilename(int id);
-char *id2filename(int id);
-int checkisdigit(char* arg);
-
-#endif
