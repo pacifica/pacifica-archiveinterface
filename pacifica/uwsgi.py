fd3f54b5453382563146a1b9ba35241524bfa65a
knig880 <christopher.knight@pnnl.gov>
2015-11-04 11:02:43 -0800

    Adding the application/config files for using the uwsgi server to run the application

---
+++ pacifica/uwsgi.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+from archive_interface import ArchiveGenerator
+import ConfigParser
+import os.path
+
+
+def application(env, start_response):
+	#read the config settings from the application.ini file
+	settings = ConfigParser.ConfigParser()
+	settings.read(os.path.dirname(os.path.realpath(__file__)) +
+		"/application.ini")
+	typed = settings.get('BackendSettings','type')
+	prefix = settings.get('BackendSettings','prefix')
+	user = settings.get('BackendSettings','user')
+	auth = settings.get('BackendSettings','auth')
+
+	GENERATOR = ArchiveGenerator(typed, prefix, user, auth)
+	return GENERATOR.pacifica_archiveinterface(env, start_response)
