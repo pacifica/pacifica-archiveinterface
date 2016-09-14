2200187ee0a46001f40c6cc01cf9dc8f350a01a3
Christopher Knight <christopher.knight@pnnl.gov>
2016-09-14 14:53:48 -0700

    Fixing pylint Issues in the archive interface

--- pacifica/archive_interface.py
+++ pacifica/archive_interface.py
@@ -4,7 +4,6 @@ from json import dumps
 from os import path
 from sys import stderr
 import sys
-import dl
 import doctest
 import time
 import datetime
@@ -12,9 +11,10 @@ import email.utils as eut
 try:
     #Due to an update in hpss version we need to lazy load the linked
     #c types.  Doing this dlopen flags. 8 is the UNIX flag Integer for
-    #RTLD_DEEPBIND
+    #RTLD_DEEPBIND. RTLD_LAZY is defined as 1 in a Linux environment
+    RTLD_LAZY = 1
     RTLD_DEEPBIND = 8
-    sys.setdlopenflags(dl.RTLD_LAZY | RTLD_DEEPBIND)
+    sys.setdlopenflags(RTLD_LAZY | RTLD_DEEPBIND)
     from pacifica.hpss_ctypes import HPSSClient
     from pacifica.hpss_ctypes import HPSSStatus
     HAVE_HPSS = True
@@ -273,4 +273,3 @@ class ArchiveGenerator(object):
 
 if __name__ == "__main__":
     doctest.testmod(verbose=True)
-
