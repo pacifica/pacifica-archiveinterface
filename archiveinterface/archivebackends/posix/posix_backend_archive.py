a2a3ca27eab199f3db76f02e0ce257323a20d1d2
David Brown <dmlb2000@gmail.com>
2017-06-03 08:35:01 -0700

    fix this issue

--- archiveinterface/archivebackends/posix/posix_backend_archive.py
+++ archiveinterface/archivebackends/posix/posix_backend_archive.py
@@ -7,6 +7,7 @@ backend.
 
 import os
 from archiveinterface.archive_utils import un_abs_path, read_config_value
+from archiveinterface.id2filename import id2filename
 from archiveinterface.archive_interface_error import ArchiveInterfaceError
 from archiveinterface.archivebackends.posix.extendedfile import ExtendedFile
 from archiveinterface.archivebackends.abstract.abstract_backend_archive \
