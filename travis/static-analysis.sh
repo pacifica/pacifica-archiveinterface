3fbf58bdac162a6df5e6d00891cc889f2f1c6a1d
David Brown <dmlb2000@gmail.com>
2017-10-02 15:57:11 -0700

    add pylintrc and one pylint for travis testing

--- travis/static-analysis.sh
+++ travis/static-analysis.sh
@@ -1,23 +1,25 @@
 #!/bin/bash -xe
 
 pre-commit run -a
-pylint scripts/archiveinterfaceserver.py
-pylint archiveinterface.archive_interface
-pylint archiveinterface.archive_interface_responses
-pylint archiveinterface.archive_interface_error
-pylint archiveinterface.archive_utils
-pylint archiveinterface.id2filename
-pylint archiveinterface.archivebackends.archive_backend_factory
-pylint archiveinterface.archivebackends.abstract.abstract_backend_archive
-pylint archiveinterface.archivebackends.abstract.abstract_status
-pylint archiveinterface.archivebackends.posix.posix_backend_archive
-pylint archiveinterface.archivebackends.posix.posix_status
-pylint archiveinterface.archivebackends.posix.extendedfile
-pylint archiveinterface.archivebackends.hpss.hpss_backend_archive
-pylint archiveinterface.archivebackends.hpss.hpss_extended
-pylint archiveinterface.archivebackends.hpss.hpss_status
-pylint archiveinterface.archivebackends.oracle_hms_sideband.extended_hms_sideband
-pylint archiveinterface.archivebackends.oracle_hms_sideband.hms_sideband_backend_archive
-pylint archiveinterface.archivebackends.oracle_hms_sideband.hms_sideband_status
-pylint archiveinterface.archivebackends.oracle_hms_sideband.hms_sideband_orm
-pylint post_deployment_tests/deployment_test.py
+pylint \
+    --rcfile=pylintrc \
+    scripts/archiveinterfaceserver.py \
+    archiveinterface.archive_interface \
+    archiveinterface.archive_interface_responses \
+    archiveinterface.archive_interface_error \
+    archiveinterface.archive_utils \
+    archiveinterface.id2filename \
+    archiveinterface.archivebackends.archive_backend_factory \
+    archiveinterface.archivebackends.abstract.abstract_backend_archive \
+    archiveinterface.archivebackends.abstract.abstract_status \
+    archiveinterface.archivebackends.posix.posix_backend_archive \
+    archiveinterface.archivebackends.posix.posix_status \
+    archiveinterface.archivebackends.posix.extendedfile \
+    archiveinterface.archivebackends.hpss.hpss_backend_archive \
+    archiveinterface.archivebackends.hpss.hpss_extended \
+    archiveinterface.archivebackends.hpss.hpss_status \
+    archiveinterface.archivebackends.oracle_hms_sideband.extended_hms_sideband \
+    archiveinterface.archivebackends.oracle_hms_sideband.hms_sideband_backend_archive \
+    archiveinterface.archivebackends.oracle_hms_sideband.hms_sideband_status \
+    archiveinterface.archivebackends.oracle_hms_sideband.hms_sideband_orm \
+    post_deployment_tests/deployment_test.py
