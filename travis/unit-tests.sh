fbc11861834720cdd2e3550e138d2dc6459450a5
David Brown <dmlb2000@gmail.com>
2017-12-05 12:35:34 -0800

    try running with pytest

--- travis/unit-tests.sh
+++ travis/unit-tests.sh
@@ -4,7 +4,7 @@ export PYTHONPATH=$PWD
 coverage run \
     --include='archiveinterface/*' \
     --omit='archiveinterface/archivebackends/abstract/*' \
-    archiveinterface/archive_interface_unit_tests.py -v
+    -m pytest archiveinterface/archive_interface_unit_tests.py -v
 coverage report -m --fail-under 90
 if [[ $CODECLIMATE_REPO_TOKEN ]] ; then
   codeclimate-test-reporter
