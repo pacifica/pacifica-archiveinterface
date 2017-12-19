44d4e7ad59c55eb8364c27d8543a7c33b53366c6
David Brown <dmlb2000@gmail.com>
2017-12-19 15:15:27 -0800

    let's try making scm version strings

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 """Setup and install the archive interface with hpss."""
 import sys
-from pip.req import parse_requirements
+# pylint: disable=no-name-in-module
+# pylint: disable=import-error
 from distutils.core import setup, Extension
+# pylint: enable=import-error
+# pylint: enable=no-name-in-module
+from pip.req import parse_requirements
 
 # parse_requirements() returns generator of pip.req.InstallRequirement objects
 INSTALL_REQS = parse_requirements('requirements.txt', session='hack')
 
 HPSS = Extension('archiveinterface.archivebackends.hpss._hpssExtensions',
-                 sources=['archiveinterface/archivebackends/hpss/hpssExtensions.c'],
+                 sources=[
+                     'archiveinterface/archivebackends/hpss/hpssExtensions.c'],
                  include_dirs=['/opt/hpss/include'],
                  library_dirs=['/opt/hpss/lib'],
                  libraries=['hpss', 'tirpc'],
@@ -20,7 +26,8 @@ if '--hpss' in sys.argv:
     sys.argv.remove('--hpss')
 
 setup(name='PacificaArchiveInterface',
-      version='1.0',
+      use_scm_version=True,
+      setup_requires=['setuptools_scm'],
       description='Pacifica Archive Interface',
       author='David Brown',
       author_email='david.brown@pnnl.gov',
@@ -31,7 +38,7 @@ setup(name='PacificaArchiveInterface',
                 'archiveinterface.archivebackends.oracle_hms_sideband'],
       scripts=['ArchiveInterfaceServer.py'],
       entry_point={
-        'console_scripts': ['ArchiveInterface=archiveinterface:main'],
+          'console_scripts': ['ArchiveInterface=archiveinterface:main'],
       },
       install_requires=[str(ir.req) for ir in INSTALL_REQS],
       ext_modules=EXT_MODULES)
