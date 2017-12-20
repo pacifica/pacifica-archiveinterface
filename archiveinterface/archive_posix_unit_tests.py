5e5e97135191cc4441c2daf18bf9605b015cca09
Christopher Knight <christopher.knight@pnnl.gov>
2017-12-20 14:13:59 -0800

    Fixing tests to not have pylint errors

--- archiveinterface/archive_posix_unit_tests.py
+++ archiveinterface/archive_posix_unit_tests.py
@@ -106,6 +106,7 @@ class TestPosixBackendArchive(unittest.TestCase):
     def test_posix_backend_open_errors(self):
         """Test opening a file from posix backend with errors."""
         backend = PosixBackendArchive('/tmp')
+        mode = 'w'
         def close_error():
             """Raise an error on close."""
             raise ArchiveInterfaceError('this is an error')
@@ -132,14 +133,14 @@ class TestPosixBackendArchive(unittest.TestCase):
             hit_exception = True
         self.assertTrue(hit_exception)
 
-    def test_posix_backend_open_id2filename(self):
-        """Test opening a file from posix backend twice."""
         backend = PosixBackendArchive('/tmp')
+        mode = 'w'
         my_file = backend.open('/a/b/d', mode)
         set_config_name('test_configs/posix-id2filename.cfg')
         backend = PosixBackendArchive('/tmp')
         my_file = backend.open(12345, mode)
         set_config_name('config.cfg')
+        my_file.close()
 
     def test_posix_backend_close(self):
         """Test closing a file from posix backend."""
