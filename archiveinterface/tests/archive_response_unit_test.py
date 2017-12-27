ee86936f9ae1bdd0a068d1d72803e02c72492d3c
Christopher Knight <christopher.knight@pnnl.gov>
2017-12-27 15:37:21 -0800

    Adding more tests for responses

--- archiveinterface/tests/archive_response_unit_test.py
+++ archiveinterface/tests/archive_response_unit_test.py
@@ -4,6 +4,7 @@
 import unittest
 import json
 import archiveinterface.archive_interface_responses as interface_responses
+from archiveinterface.archivebackends.posix.posix_status import PosixStatus
 
 
 class TestInterfaceResponses(unittest.TestCase):
@@ -28,6 +29,62 @@ class TestInterfaceResponses(unittest.TestCase):
         response = resp.unknown_exception(self.start_response)
         jsn = json.loads(json.dumps(response))
         self.assertEqual(jsn['message'], 'Unknown Exception Occured')
+
+    def test_put_response(self):
+        """Test response for successful put."""
+        resp = interface_responses.Responses()
+        response = resp.successful_put_response(self.start_response, 36)
+        jsn = json.loads(json.dumps(response))
+        self.assertEqual(jsn['message'], 'File added to archive')
+        self.assertEqual(jsn['total_bytes'], 36)
+
+    def test_patch_response(self):
+        """Test response for successful patch."""
+        resp = interface_responses.Responses()
+        response = resp.file_patch(self.start_response)
+        jsn = json.loads(json.dumps(response))
+        self.assertEqual(jsn['message'], 'File Moved Successfully')
+
+    def test_working_response(self):
+        """Test response for successful archive working."""
+        resp = interface_responses.Responses()
+        response = resp.archive_working_response(self.start_response)
+        jsn = json.loads(json.dumps(response))
+        self.assertEqual(jsn['message'], 'Pacifica Archive Interface Up and Running')
+
+    def test_file_stage(self):
+        """Test response for successful stage."""
+        resp = interface_responses.Responses()
+        response = resp.file_stage(self.start_response, 'mytestfile.txt')
+        jsn = json.loads(json.dumps(response))
+        self.assertEqual(jsn['message'], 'File was staged')
+        self.assertEqual(jsn['file'], 'mytestfile.txt')
+
+    def test_patch_error(self):
+        """Test response for patch error."""
+        resp = interface_responses.Responses()
+        response = resp.json_patch_error_response(self.start_response)
+        jsn = json.loads(json.dumps(response))
+        self.assertEqual(jsn['message'], 'JSON content could not be read')
+
+    def test_archive_exception(self):
+        """Test response for patch error."""
+        resp = interface_responses.Responses()
+        response = resp.archive_exception(self.start_response, 'random_error_message', 'notHEAD')
+        jsn = json.loads(json.dumps(response))
+        self.assertEqual(jsn['message'], 'random_error_message')
+        response = resp.archive_exception(self.start_response, 'random_error_message', 'HEAD')
+        self.assertEqual(response, None)
+
+    def test_file_status(self):
+        """Test response for patch error."""
+        resp = interface_responses.Responses()
+        response = resp.file_status(self.start_response, None)
+        self.assertEqual(response, None)
+        status = PosixStatus(11, 11, 10, 10)
+        status.set_filepath('fake_path')
+        response = resp.file_status(self.start_response, status)
+        self.assertEqual(response, None)
 
 
 if __name__ == '__main__':
