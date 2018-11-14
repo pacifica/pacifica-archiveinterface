232419eec0a7a1e99af1c5b5689dd1925799fbb8
David Brown <dmlb2000@gmail.com>
2018-11-13 21:22:27 -0800

    fix the docker image to work properly (#66)
    Signed-off-by: David Brown <dmlb2000@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ uwsgi \
   --http-socket $PACIFICA_AAPI_ADDRESS:$PACIFICA_AAPI_PORT \
   --master \
   --die-on-term \
-  --wsgi-file /usr/src/app/archiveinterface/wsgi.py "$@"
+  --module "pacifica.archiveinterface.wsgi:application" "$@"
