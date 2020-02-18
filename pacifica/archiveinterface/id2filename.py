4df78933d95d2ec0c367376e8d9b6d451fae7615
David Brown <dmlb2000@gmail.com>
2020-02-18 09:30:31 -0800

    Fix #95 Add id2filename Docs (#108)
    This adds some docs for `id2filename` and a reverse method that
    turns a valid path into and `ID`.
    
    Signed-off-by: David Brown <dmlb2000@gmail.com>

--- pacifica/archiveinterface/id2filename.py
+++ pacifica/archiveinterface/id2filename.py
@@ -1,11 +1,29 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-"""Module to Convert an integer id to a filepath for storage system."""
+"""
+Module to convert an integer id to a filepath for storage system.
+
+Mathematically, this is a bijective_ set of methods. This means that
+running `filename2id(id2filename(ID))` must return the same `ID`
+passed and conversely `id2filename(filename2id(PATH))` must return
+the same `PATH` passed.
+
+Example usage:
+
+::
+
+  >>> id2filename(1234)
+  '/d2/4d2'
+  >>> filename2id('/d2/4d2')
+  1234
+
+.. _bijective: https://en.wikipedia.org/wiki/Bijection
+"""
 from __future__ import print_function
 import sys
 
 
-def id2dirandfilename(fileid):
+def id2filename(fileid):
     """Algorithm for getting filepath from an integer id."""
     hexfileid = '{0:x}'.format(fileid)
     directories = ''
@@ -22,9 +40,11 @@ def id2dirandfilename(fileid):
     return filepath
 
 
-def id2filename(fileid):
-    """Will return the filepath associated to passed fileid."""
-    return id2dirandfilename(fileid)
+def filename2id(path):
+    """Convert a filepath to and ID."""
+    if path[:6] == '/file.':
+        return int(path[6:], 16)
+    return int(path.split('/')[-1], 16)
 
 
 if __name__ == '__main__':  # pragma: no cover
